0999a46a41d85715b236bef8192070c2e5b2f5bf
tgrander <treygranderson@gmail.com>
2018-02-11 22:46:13 -0500

    refactor: removeItemFromArrat util function uses Array.slice() to remove item by index

--- src/utilities/removeItemFromArray.js
+++
@@ -1,2 +0,0 @@
-export default (array, item) =>
-  array.filter(el => el !== item && el);
---
+++ src/utilities/removeItemFromArrayByIndex.js
@@ -0,0 +1,4 @@
+export default (array, index) => ([
+  ...array.slice(0, index),
+  ...array.slice(index + 1),
+]);
